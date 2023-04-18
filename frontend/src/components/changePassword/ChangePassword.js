0096e7bc342332a1b5116957bc983605d380485a
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-04-18 23:50:19 +0100

    prettier

--- frontend/src/components/changePassword/ChangePassword.js
+++ frontend/src/components/changePassword/ChangePassword.js
@@ -1,70 +1,156 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { changePassword } from '../../services/authService';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { changePassword } from "../../services/authService";
 
-
-const initialState ={
-    oldPassword: "",
-    password: "",
-    password2: "",
-  }
+const initialState = {
+  oldPassword: "",
+  password: "",
+  password2: "",
+};
 
 const ChangePassword = () => {
-    const navigate = useNavigate()
-const [formData, setformData] = useState(initialState);
+  const navigate = useNavigate();
+  const [formData, setformData] = useState(initialState);
   const { oldPassword, password, password2 } = formData;
 
   const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    setformData ({ ...formData, [name]: value})
-  }
- 
-const changePass = async (e) => {
-    e.preventDefault()
+    const { name, value } = e.target;
+    setformData({ ...formData, [name]: value });
+  };
+
+  const changePass = async (e) => {
+    e.preventDefault();
 
     if (password !== password2) {
-        return toast.error("New passwords donot match")
+      return toast.error("New passwords donot match");
     }
 
     const formData = {
-        oldPassword,
-        password
-    }
-    const data =await changePassword(formData)
-    toast.success(data)
-    navigate("/profile")
-}
-
+      oldPassword,
+      password,
+    };
+    const data = await changePassword(formData);
+    toast.success(data);
+    navigate("/profile");
+  };
 
   return (
-    <div className=" shadow-2xl m-2 p-2" style={{ border:"2px solid var(--color-green)"}}>
-        <div className=" bg-primary-content">
-            <div style={{color:"var(--color-black)", fontSize:"25px", borderBottom:"2px solid var(--color-green)"}}>Change Password:</div> 
+    <div
+      className="p-2 m-2 shadow-2xl "
+      style={{ border: "2px solid var(--color-green)" }}
+    >
+      <div className=" bg-primary-content">
+        <div
+          style={{
+            color: "var(--color-black)",
+            fontSize: "25px",
+            borderBottom: "2px solid var(--color-green)",
+          }}
+        >
+          Change Password:
         </div>
-        <form onSubmit={changePass} className="p-2" >
-                <label className="input-group ">
-                    <span className='label-text p-1' style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize",}}>Old Password</span> 
-                    <input type="password" placeholder='Old Password' name='oldPassword' value={oldPassword} onChange = {handleInputChange} className="input input-bordered" style={{background:"var(--color-l-green)", width:"100%", color:"var(--color-black)",}}/>
-                </label>   
+      </div>
+      <form onSubmit={changePass} className="p-2">
+        <label className="input-group ">
+          <span
+            className="p-1 label-text"
+            style={{
+              background: "var(--color-green)",
+              color: "var(--color-white)",
+              textTransform: "capitalize",
+            }}
+          >
+            Old Password
+          </span>
+          <input
+            type="password"
+            placeholder="Old Password"
+            name="oldPassword"
+            value={oldPassword}
+            onChange={handleInputChange}
+            className="input input-bordered"
+            style={{
+              background: "var(--color-l-green)",
+              width: "100%",
+              color: "var(--color-black)",
+            }}
+          />
+        </label>
 
-            <div style={{borderBottom:"2px solid var(--color-green)"}} className='p-2'/>
-                <label className="input-group ">
-                    <span className='label-text p-1' style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize",}}>New Password</span> 
-                    <input type="password" placeholder='New Password' name='password' value={password} onChange = {handleInputChange} className="input input-bordered" style={{background:"var(--color-l-green)", width:"100%", color:"var(--color-black)",}}/>
-                </label>
+        <div
+          style={{ borderBottom: "2px solid var(--color-green)" }}
+          className="p-2"
+        />
+        <label className="input-group ">
+          <span
+            className="p-1 label-text"
+            style={{
+              background: "var(--color-green)",
+              color: "var(--color-white)",
+              textTransform: "capitalize",
+            }}
+          >
+            New Password
+          </span>
+          <input
+            type="password"
+            placeholder="New Password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            className="input input-bordered"
+            style={{
+              background: "var(--color-l-green)",
+              width: "100%",
+              color: "var(--color-black)",
+            }}
+          />
+        </label>
 
-            <div style={{borderBottom:"2px solid var(--color-green)"}} className='p-2'/>
-                <label className="input-group ">
-                    <span className='label-text p-1' style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize",}}>Confirm Password</span> 
-                    <input type="password" placeholder='Confirm Password' name='password2' value={password2} onChange = {handleInputChange} className="input input-bordered" style={{background:"var(--color-l-green)", width:"100%", color:"var(--color-black)",}}/>
-                </label>
+        <div
+          style={{ borderBottom: "2px solid var(--color-green)" }}
+          className="p-2"
+        />
+        <label className="input-group ">
+          <span
+            className="p-1 label-text"
+            style={{
+              background: "var(--color-green)",
+              color: "var(--color-white)",
+              textTransform: "capitalize",
+            }}
+          >
+            Confirm Password
+          </span>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={handleInputChange}
+            className="input input-bordered"
+            style={{
+              background: "var(--color-l-green)",
+              width: "100%",
+              color: "var(--color-black)",
+            }}
+          />
+        </label>
 
-            <button type='submit' className="btn mt-3" style={{background:"var(--color-green)", color:"var(--color-l-green)"}}>Change Password</button>
-
-        </form>
+        <button
+          type="submit"
+          className="mt-3 btn"
+          style={{
+            background: "var(--color-green)",
+            color: "var(--color-l-green)",
+          }}
+        >
+          Change Password
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ChangePassword
+export default ChangePassword;
