6c329a6bccec04985ad7e19e652997fffb08ca84
Oke Babatunde <103512075+Twise007@users.noreply.github.com>
2023-07-02 00:32:22 +0100

    using prettier

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,53 +1,146 @@
-import React from 'react';
-import { TfiBookmarkAlt } from "react-icons/tfi"
-import { Link }from "react-router-dom";
-import Homepic from '../../assets/home.jpg';
-import { ShowOnLogin, ShowOnLogout } from '../../components/protect/HiddenLinks';
-
-
+import React from "react";
+import { TfiBookmarkAlt } from "react-icons/tfi";
+import { Link } from "react-router-dom";
+import Homepic from "../../assets/home.jpg";
+import {
+  ShowOnLogin,
+  ShowOnLogout,
+} from "../../components/protect/HiddenLinks";
 
 const home = () => {
-  return (   
+  return (
     <div>
-        <div className="navbar" style={{top: '0px', position: 'sticky',background:"var(--color-green)",zIndex:'10', color:"var(--color-black)"}} >
-        <div className="flex-1" >
-            <b className="btn normal-case text-xl" style={{background:'var(--color-l-green)', cursor:"none"}}>
-                <TfiBookmarkAlt  size={35} style={{color:"var(--color-black)"}}/>
-                <p className='m-2' style={{color:"var(--color-black)"}}>E-ventory App</p>
-            </b>
+      <div
+        className="navbar"
+        style={{
+          top: "0px",
+          position: "sticky",
+          background: "var(--color-green)",
+          zIndex: "10",
+          color: "var(--color-black)",
+        }}
+      >
+        <div className="flex-1">
+          <b
+            className="text-xl normal-case btn"
+            style={{ background: "var(--color-l-green)", cursor: "none" }}
+          >
+            <TfiBookmarkAlt size={35} style={{ color: "var(--color-black)" }} />
+            <p className="m-2" style={{ color: "var(--color-black)" }}>
+              E-ventory App
+            </p>
+          </b>
         </div>
         <div className="flex-none">
-            <ul className="menu menu-horizontal px-1">
-                <ShowOnLogout>
-                <li><Link to='/register' style={{border:"1px solid", marginLeft:"10px", background:"var(--color-l-green)"}}>Register</Link></li>
-                </ShowOnLogout>
-                <ShowOnLogout>
-                <li><Link to='/login' style={{border:"1px solid", marginLeft:"10px", background:"var(--color-l-green)"}}>Login</Link></li>
-                </ShowOnLogout>
-                <ShowOnLogin>
-                <li><Link to='/dashboard' style={{border:"1px solid", marginLeft:"10px", background:"var(--color-l-green)"}}>Dashboard</Link></li>
-                </ShowOnLogin>
-            </ul>
+          <ul className="px-1 menu menu-horizontal">
+            <ShowOnLogout>
+              <li>
+                <Link
+                  to="/register"
+                  style={{
+                    border: "1px solid",
+                    marginLeft: "10px",
+                    background: "var(--color-l-green)",
+                  }}
+                >
+                  Register
+                </Link>
+              </li>
+            </ShowOnLogout>
+            <ShowOnLogout>
+              <li>
+                <Link
+                  to="/login"
+                  style={{
+                    border: "1px solid",
+                    marginLeft: "10px",
+                    background: "var(--color-l-green)",
+                  }}
+                >
+                  Login
+                </Link>
+              </li>
+            </ShowOnLogout>
+            <ShowOnLogin>
+              <li>
+                <Link
+                  to="/dashboard"
+                  style={{
+                    border: "1px solid",
+                    marginLeft: "10px",
+                    background: "var(--color-l-green)",
+                  }}
+                >
+                  Dashboard
+                </Link>
+              </li>
+            </ShowOnLogin>
+          </ul>
         </div>
-        </div>
+      </div>
 
-        {/*Hero section */}
-        <div className="hero min-h-screen" style={{backgroundImage: `url(${Homepic})`}}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div  className="hero-content text-center text-neutral-content" style={{}}>
-                <div  className="">
-                <h1 className='text-4xl font-bold m-10' style={{color:'var(--color-white)'}}>Inventory system to keep, control and manage your products records in real time.</h1>
-                <p className="mb-3" style={{color:'var(--color-white)', fontSize:"25px", fontFamily:"fantasy"}}><span style={{fontSize:"35px"}}>14 -</span> Brand owners</p>
-                <p className="mb-3" style={{color:'var(--color-white)', fontSize:"25px", fontFamily:"fantasy"}}><span style={{fontSize:"35px"}}>23 -</span> Active users</p>
-                <p className="mb-3" style={{color:'var(--color-white)', fontSize:"25px", fontFamily:"fantasy"}}><span style={{fontSize:"35px"}}>50 -</span> Partners</p>
-                <button className="btn" style={{background:"var(--color-l-green)", color:'var(--color-black)'}}>
-                    <Link to='/register'>Free Trail 1 Month</Link>
-                </button>
-                </div>
-            </div>
+      {/*Hero section */}
+      <div
+        className="min-h-screen hero"
+        style={{ backgroundImage: `url(${Homepic})` }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div
+          className="text-center hero-content text-neutral-content"
+          style={{}}
+        >
+          <div className="">
+            <h1
+              className="m-10 text-4xl font-bold"
+              style={{ color: "var(--color-white)" }}
+            >
+              Inventory system to keep, control and manage your products records
+              in real time.
+            </h1>
+            <p
+              className="mb-3"
+              style={{
+                color: "var(--color-white)",
+                fontSize: "25px",
+                fontFamily: "fantasy",
+              }}
+            >
+              <span style={{ fontSize: "35px" }}>14 -</span> Brand owners
+            </p>
+            <p
+              className="mb-3"
+              style={{
+                color: "var(--color-white)",
+                fontSize: "25px",
+                fontFamily: "fantasy",
+              }}
+            >
+              <span style={{ fontSize: "35px" }}>23 -</span> Active users
+            </p>
+            <p
+              className="mb-3"
+              style={{
+                color: "var(--color-white)",
+                fontSize: "25px",
+                fontFamily: "fantasy",
+              }}
+            >
+              <span style={{ fontSize: "35px" }}>50 -</span> Partners
+            </p>
+            <button
+              className="btn"
+              style={{
+                background: "var(--color-l-green)",
+                color: "var(--color-black)",
+              }}
+            >
+              <Link to="/register">Free Trail 1 Month</Link>
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 };
 
-export default home
+export default home;
--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -7,89 +7,198 @@ import { SET_NAME, SET_USER } from "../../redux/features/auth/authSlice";
 import { getUser } from "../../services/authService";
 
 const Profile = () => {
+  useRedirectLoggedOutUser("/login");
+  const dispatch = useDispatch();
 
-    useRedirectLoggedOutUser("/login");
-    const dispatch = useDispatch();
-  
-    const [profile, setProfile] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-  
-    useEffect(() => {
-      console.log("Getting user");
-      setIsLoading(true);
-      async function getUserData() {
-        const data = await getUser();
-        console.log(data);
-  
-        setProfile(data);
-        setIsLoading(false);
-        await dispatch(SET_USER(data));
-        await dispatch(SET_NAME(data.name));
-      }
-      getUserData();
-    }, [dispatch]);
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    console.log("Getting user");
+    setIsLoading(true);
+    async function getUserData() {
+      const data = await getUser();
+      console.log(data);
+
+      setProfile(data);
+      setIsLoading(false);
+      await dispatch(SET_USER(data));
+      await dispatch(SET_NAME(data.name));
+    }
+    getUserData();
+  }, [dispatch]);
 
   return (
     <>
-    <div className="">
+      <div className="">
         <div className="navbar bg-primary-content">
-            <div className="flex-1" style={{color:"var(--color-black)", fontSize:"25px", borderBottom:"2px solid var(--color-green)"}}>Profile:</div> 
+          <div
+            className="flex-1"
+            style={{
+              color: "var(--color-black)",
+              fontSize: "25px",
+              borderBottom: "2px solid var(--color-green)",
+            }}
+          >
+            Profile:
+          </div>
         </div>
         {isLoading && <SpinnerImg />}
         <>
-        {!isLoading && profile === null ? (
-          <p>Something went wrong, please reload the page...</p>
-        ) : (
-        <div className="pt-10 pb-10" style={{background:"var(--color-off-white)",}}>
-            <div className="shadow-2xl" style={{ background:"var(--color-white)",}}>
+          {!isLoading && profile === null ? (
+            <p>Something went wrong, please reload the page...</p>
+          ) : (
+            <div
+              className="pt-10 pb-10"
+              style={{ background: "var(--color-off-white)" }}
+            >
+              <div
+                className="shadow-2xl"
+                style={{ background: "var(--color-white)" }}
+              >
                 <div className="p-5">
-                    <img src={profile?.photo} alt='profilepic' className=" rounded-full shadow-2xl p-5" style={{width:"22pc", height:"22pc", background:"var(--color-l-green)"}}/>
-                    <div className=" shadow-2xl">
-                    <div className="m-2 card"> 
-                        <label className="input-group pt-3">
-                            <span style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize"}}>name</span>
-                            <p className="p-2 w-full " style={{background:"var(--color-l-green)", color:"var(--color-black)", textTransform:"capitalize"}}>{profile?.name}</p>
+                  <img
+                    src={profile?.photo}
+                    alt="profilepic"
+                    className="p-5 rounded-full shadow-2xl "
+                    style={{
+                      width: "22pc",
+                      height: "22pc",
+                      background: "var(--color-l-green)",
+                    }}
+                  />
+                  <div className="shadow-2xl ">
+                    <div className="m-2 card">
+                      <label className="pt-3 input-group">
+                        <span
+                          style={{
+                            background: "var(--color-green)",
+                            color: "var(--color-white)",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          name
+                        </span>
+                        <p
+                          className="w-full p-2 "
+                          style={{
+                            background: "var(--color-l-green)",
+                            color: "var(--color-black)",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          {profile?.name}
+                        </p>
+                      </label>
+
+                      <div
+                        style={{ borderBottom: "2px solid var(--color-green)" }}
+                        className="p-2"
+                      />
+                      <div className="form-control">
+                        <label className="pt-3 input-group">
+                          <span
+                            style={{
+                              background: "var(--color-green)",
+                              color: "var(--color-white)",
+                              textTransform: "capitalize",
+                            }}
+                          >
+                            email
+                          </span>
+                          <span
+                            className="p-2 "
+                            style={{
+                              background: "var(--color-l-green)",
+                              color: "var(--color-black)",
+                              textTransform: "capitalize",
+                            }}
+                          >
+                            {profile?.email}
+                          </span>
                         </label>
+                      </div>
 
-                            <div style={{borderBottom:"2px solid var(--color-green)"}} className='p-2'/>
-                            <div className="form-control">
-                            <label className="input-group pt-3">
-                                <span style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize"}}>email</span>
-                                <span className="p-2 " style={{background:"var(--color-l-green)", color:"var(--color-black)", textTransform:"capitalize"}}>{profile?.email}</span>
-                            </label>
-                            </div>
+                      <div
+                        style={{ borderBottom: "2px solid var(--color-green)" }}
+                        className="p-2"
+                      />
+                      <div className="form-control">
+                        <label className="pt-3 input-group">
+                          <span
+                            style={{
+                              background: "var(--color-green)",
+                              color: "var(--color-white)",
+                              textTransform: "capitalize",
+                            }}
+                          >
+                            phone
+                          </span>
+                          <p
+                            className="w-full p-2 "
+                            style={{
+                              background: "var(--color-l-green)",
+                              color: "var(--color-black)",
+                              textTransform: "capitalize",
+                            }}
+                          >
+                            {profile?.phone}
+                          </p>
+                        </label>
+                      </div>
 
+                      <div
+                        style={{ borderBottom: "2px solid var(--color-green)" }}
+                        className="p-2"
+                      />
+                      <label className="pt-3 input-group">
+                        <span
+                          style={{
+                            background: "var(--color-green)",
+                            color: "var(--color-white)",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          bio
+                        </span>
+                        <p
+                          className="w-full p-2 "
+                          style={{
+                            background: "var(--color-l-green)",
+                            color: "var(--color-black)",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          {profile?.bio}
+                        </p>
+                      </label>
+                      <div
+                        style={{ borderBottom: "2px solid var(--color-green)" }}
+                        className="p-2"
+                      />
 
-                            <div style={{borderBottom:"2px solid var(--color-green)"}} className='p-2'/>
-                            <div className="form-control">
-                            <label className="input-group pt-3">
-                                <span style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize"}}>phone</span>
-                                <p className="p-2 w-full " style={{background:"var(--color-l-green)", color:"var(--color-black)", textTransform:"capitalize"}}>{profile?.phone}</p>
-                            </label>
-                            </div>
-
-                            <div style={{borderBottom:"2px solid var(--color-green)"}} className='p-2'/>
-                            <label className="input-group pt-3">
-                                <span style={{background:"var(--color-green)", color:"var(--color-white)", textTransform:"capitalize"}}>bio</span>
-                                <p className="p-2 w-full " style={{background:"var(--color-l-green)", color:"var(--color-black)", textTransform:"capitalize"}}>{profile?.bio}</p>
-                            </label>
-                            <div style={{borderBottom:"2px solid var(--color-green)"}} className='p-2'/>
-                            
-                            <Link to="/edit-profile">
-                                <button type='submit' className="btn mt-3" style={{background:"var(--color-green)", color:"var(--color-l-green)"}}>Edit Profile</button>
-                            </Link>
-                        </div>
-
+                      <Link to="/edit-profile">
+                        <button
+                          type="submit"
+                          className="mt-3 btn"
+                          style={{
+                            background: "var(--color-green)",
+                            color: "var(--color-l-green)",
+                          }}
+                        >
+                          Edit Profile
+                        </button>
+                      </Link>
                     </div>
-                        
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-        )}
+          )}
         </>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
